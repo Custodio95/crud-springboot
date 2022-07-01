2723090d77a294177d95ea44e38eef5217943fd0
custodio95 <gabriel.custodio.ferreira@gmail.com>
2022-06-30 22:30:07 -0300

    Create Project

---
+++ src/test/java/com/custodio/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custodio.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
